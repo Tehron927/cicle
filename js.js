57eda448548e55989f6756e37a960e6d48448fcb
Tehron927 <tehronmelikov2@gmail.com>
2024-02-10 12:55:19 +0500

    cicle  commit

---
+++ js.js
@@ -0,0 +1,21 @@
+const colors = ["red", "green", "blue", "yellow"];
+
+// Вывод элементов
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
+
+console.log("Квадраты чисел от 1 до 10");
+for (let i = 1; i <= 10; i++) {
+    console.log(i * i);
+}
+
+
+console.log("Нечетные числа от 1 до 20");
+for (let i = 1; i <= 20; i += 2) {
+    console.log(i);
+}
+
+
+
+
